78925b125cf9611ada28064b7aafff5c3255cb7f
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-04-10 22:15:24 -0300

    Add syntax for variable initialization

--- compile.js
+++ compile.js
@@ -1,8 +1,7 @@
 let code = `
-let fullName = "Hello world";
 class Name {
-	let lastName = 123;
-	let firstName;
+	let lastName => "Mateus";
+	let firstName => "Sarmento";
 }
 `;
 
@@ -29,10 +28,28 @@ function parse(tokens) {
 		if (token.type === 'let') {
 			if (token.next && token.next.type === 'identifier') {
 				token = token.next;
-				variable = {type: 'declared_variable', name: token.name}; 
-				if (token.next && token.next.type === "semicolon") {
+				let variableName = token.name;
+				if (token.next && token.next.type === ";") {
+					variable = {type: 'declared_variable', name: variableName, value: "null"};
 					token = token.next;
+				} else if (token.next && token.next.type === "=") {
+					token = token.next.next;
+					if (token && token.type === "literal") {
+						variable = {type: 'declared_variable', name: variableName, value: token.text};
+						token = token.next;
+					} else if (token && token.type === "identifier") {
+						let identifier = ast.find(e => e.type === "declared_variable" && e.name === token.name);
+						if (identifier) {
+							variable = {type: 'declared_variable', name: variableName, value: identifier.value};
+						} else {
+							log(`Error: Unknown identifier "${token.name}"`, token);
+						}
+						token = token.next;
+					} else {
+						log(`Error: expected value for variable initialization`, token);
+					}
 				} else {
+					variable = {type: 'declared_variable', name: variableName, value: "null"};
 					log(`Error: missing semicolon after variable identifier`, token);
 				}
 			} else {
@@ -49,13 +66,13 @@ function parse(tokens) {
 			if (token.next && token.next.type === "identifier") {
 				token = token.next;
 				let className = token.name;
-				if (token.next && token.next.type === "semicolon") {
+				if (token.next && token.next.type === ";") {
 					classtype = {type: "declared_class", name: className, body: []};
 					token = token.next.next;
-				} else if (token.next && token.next.type === "open_curly_brackets") {
+				} else if (token.next && token.next.type === "{") {
 					token = token.next.next;
 					let variables = [], variable;
-					while (token.type !== "close_curly_brackets") {
+					while (token.type !== "}") {
 						[token, variable] = processVariableDeclaration(token);
 						token = token.next;
 						if (variable) variables.push(variable);
